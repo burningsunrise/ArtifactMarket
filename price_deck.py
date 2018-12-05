9a3d6221b25cc82b4029f1f122ecd3b242c9e81f
Unknown <nvreaver@gmail.com>
2018-12-04 17:02:46 -0800

    inventoiry

--- price_deck.py
+++ price_deck.py
@@ -4,6 +4,14 @@ from requests.exceptions import RequestException
 from contextlib import closing
 from bs4 import BeautifulSoup
 import urllib.request, json, datetime
+import pprint
+
+# Lists
+market_name = []
+sell_price = []
+card_hash = []
+sale_listings =[]
+
 
 def simple_get(url):
     try:
@@ -25,41 +33,109 @@ def is_good_response(resp):
 def log_error(e):
     print(e)
 
-print("Welcome to the pricer!\n")
-deck_list = input("Give me that deck!: ")
-raw_html = simple_get(deck_list)
+def get_market_data():
+    global market_name
+    global sell_price
+    global card_hash
+    global sale_listings
+    url_prefix = "https://steamcommunity.com/market/search/render/?search_descriptions=0&category_583950_Rarity%5B%5D="
+    url_suffix = "&sort_dir=desc&appid=583950&norender=1&count=500"
+    url_common = "tag_Rarity_Common"
+    url_uncommon = "tag_Rarity_Uncommon"
+    url_rare = "tag_Rarity_Rare"
+    # commons
+    with urllib.request.urlopen(url_prefix + url_common + url_suffix) as url:
+        data = json.loads(url.read().decode())
 
-html = BeautifulSoup(raw_html, 'html.parser')
+    # relevant data name, sale_prince, sell_listings (how many), sell_price, sell_price_text, hash_name
+    for commons in data['results']:
+        market_name.append(commons['name'])
+        sell_price.append(commons['sell_price']/100)
+        card_hash.append(commons['hash_name'])
+        sale_listings.append(commons['sell_listings'])
 
-names = html.findAll("div", {'class': 'cardName'}, text=True)
-card_counts = html.findAll("div", {'class': 'cardCount'}, text=True)
-hash_names = html.findAll("div", {"class" : lambda L: L and L.startswith('hero color')})
+    # uncommons
+    with urllib.request.urlopen(url_prefix + url_uncommon + url_suffix) as url:
+        data = json.loads(url.read().decode())
 
-name = []
-count = []
-heroes = []
-for name_text in names:
-    name.append(name_text.text)
+    for uncommons in data['results']:
+        market_name.append(uncommons['name'])
+        sell_price.append(uncommons['sell_price']/100)
+        card_hash.append(uncommons['hash_name'])
+        sale_listings.append(uncommons['sell_listings'])
 
-for card_count in card_counts:
-    count.append(card_count.text.split('x')[1])
+    # rares
+    with urllib.request.urlopen(url_prefix + url_rare + url_suffix) as url:
+        data = json.loads(url.read().decode())
 
-for hash_name in hash_names:
-    heroes.append("1" + hash_name.get('id').split('card')[1])
+    for rares in data['results']:
+        market_name.append(rares['name'])
+        sell_price.append(rares['sell_price']/100)
+        card_hash.append(rares['hash_name'])
+        sale_listings.append(rares['sell_listings'])
 
-deck = dict(zip(name, count))
+def main():
+    print("Welcome to the pricer!\n")
+    compare = input("Use your steam invetory as a base? (Y/n): ")
+    if compare.lower() != 'n' or compare.lower() != 'no':
+        steam_inventory = input("Give me your steam name: ")
 
-with urllib.request.urlopen("https://pareidolia.vision/artifact/card_data.php") as url:
-    data = json.loads(url.read().decode())
+    if len(steam_inventory) != 0:
+        with urllib.request.urlopen('https://steamcommunity.com/id/'+steam_inventory+'/inventory/json/583950/2') as url:
+            inventory_data = json.loads(url.read().decode())
+    desc_name =[]
+    for data in inventory_data['rgDescriptions']:
+        desc_name.append(data)
 
-total = 0
-for cards in data:
-    if cards['hash_name'] in heroes:
-        print(cards['name'] + '=$' + cards['sell_price'])
-        total += float(cards['sell_price'])
-    if cards['name'] in deck:
-        print(cards['name'] + ' x' + deck[cards['name']] + '=$' + str(round(float(deck[cards['name']]) * float(cards['sell_price']), 2)) + ' | SINGLE PRICE=$' + cards['sell_price'])
-        total += round(float(deck[cards['name']]) * float(cards['sell_price']),2)
+    # inventory shit
+    inventory_name = []
+    inventory_hash = []
+    for i in range(len(inventory_data['rgDescriptions'])):
+        inventory_name.append(inventory_data['rgDescriptions'][desc_name[i]]['name'])
+        inventory_hash.append(inventory_data['rgDescriptions'][desc_name[i]]['market_hash_name'])
 
-print('=================')
-print("Total Deck Cost: " + str(total))
+    deck_list = input("Give me that deck!: ")
+    raw_html = simple_get(deck_list)
+
+    html = BeautifulSoup(raw_html, 'html.parser')
+
+    names = html.findAll("div", {'class': 'cardName'}, text=True)
+    card_counts = html.findAll("div", {'class': 'cardCount'}, text=True)
+    hash_names = html.findAll("div", {"class" : lambda L: L and L.startswith('hero color')})
+
+    name = []
+    count = []
+    heroes = []
+    for name_text in names:
+        name.append(name_text.text)
+
+    for card_count in card_counts:
+        count.append(card_count.text.split('x')[1])
+
+    for hash_name in hash_names:
+        heroes.append("1" + hash_name.get('id').split('card')[1])
+
+    deck = dict(zip(name, count))
+
+    total = 0
+    get_market_data()
+
+    # TODO figure out inventory you have name and market hash
+    # Heroes are fine to do not in because there is only one
+    # We need quantity for inventorty for market_name
+    for i in range(len(card_hash)):
+        if card_hash[i] in heroes and card_hash[i] not in inventory_hash:
+            print(market_name[i] + '=$' + str(sell_price[i]))
+            total += float(sell_price[i])
+        if market_name[i] in deck:
+            print(market_name[i] + ' x' + deck[market_name[i]] + '=$' + str(round(float(deck[market_name[i]]) * float(sell_price[i]), 2)) + ' | SINGLE PRICE=$' + str(sell_price[i]))
+            total += round(float(deck[market_name[i]]) * float(sell_price[i]),2)
+        
+
+
+    print('=================')
+    print("Total Deck Cost: " + str(total))
+    
+
+if __name__ == "__main__":
+    main()
